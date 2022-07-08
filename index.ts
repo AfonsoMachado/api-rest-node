7493165d1b16eb02139b01e7df146472adb55db0
Afonso Machado <afonso.machado@blockhub.builders>
2022-07-08 15:42:01 -0300

    initialize server

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("iok");
+import express from "express";
+
+const server = express();
+
+const port = 5000;
+
+server.listen(port, () => {
+  console.log(`Servidor on na porta ${port}`);
+});
