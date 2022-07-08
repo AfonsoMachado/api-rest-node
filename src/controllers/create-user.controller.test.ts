ea2caffce79105b7ab68c08fd49674501c962dc3
Afonso Machado <afonso.machado@blockhub.builders>
2022-07-08 20:48:33 -0300

    create mock request and response

--- src/controllers/create-user.controller.test.ts
+++ src/controllers/create-user.controller.test.ts
@@ -1,1 +1,9 @@
-describe("CreateUserController", () => {});
+import { CreateUserController } from "./create-user.controller";
+
+describe("CreateUserController", () => {
+  it("should return user id", async () => {
+    const createUserController = new CreateUserController();
+
+    const result = createUserController.handle();
+  });
+});
