ae4cb55ac5d058501c3138a94392985a1f2929c0
Afonso Machado <afonso.machado@blockhub.builders>
2022-07-08 22:03:03 -0300

    basic test

--- src/controllers/create-user.controller.test.ts
+++ src/controllers/create-user.controller.test.ts
@@ -1,9 +1,33 @@
+import { Request } from "express";
+import { createConnection, getConnection } from "typeorm";
+import { makeMockResponse } from "../utils/mocks/response.mock";
 import { CreateUserController } from "./create-user.controller";
+import createTestConnection from "../database";
 
 describe("CreateUserController", () => {
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  // afterAll(async () => {
+  //   const connection = getConnection();
+  //   await connection.query("DELETE FROM users");
+  //   await connection.close();
+  // });
+
   it("should return user id", async () => {
     const createUserController = new CreateUserController();
 
-    const result = createUserController.handle();
+    const request = {
+      body: {
+        name: "fofase",
+        email: "email",
+      },
+    } as Request;
+
+    const response = makeMockResponse();
+
+    const result = await createUserController.handle(request, response);
   });
 });
